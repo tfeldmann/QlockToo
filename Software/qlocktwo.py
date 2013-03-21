681e72fcb230adb0218e109c6c84be379fa7b981
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-03-21 15:04:02 +0100

    removed connect button in favor of a select list

--- Software/qlocktwo.py
+++ Software/qlocktwo.py
@@ -13,17 +13,28 @@ from Pattern.app import PatternApp
 
 class QlockTwo(QMainWindow):
     def __init__(self):
+        """ Initialize the main application """
         super(QlockTwo, self).__init__()
         self.ui = Ui_qlocktwo()
         self.ui.setupUi(self)
 
+        self.ui.refresh.clicked.connect(self.refresh_ports)
         self.ui.pattern.clicked.connect(self.pattern_app)
         self.ui.update.clicked.connect(self.check_updates)
 
+        self.refresh_ports()
+
+    def refresh_ports(self):
+        """ Refreshed the port list """
+        self.ui.port.clear()
+        self.ui.port.addItem('Getrennt')
+
     def pattern_app(self):
+        """ Start pattern app """
         app = PatternApp(self)
 
     def check_updates(self):
+        """ Check for updates """
         pass
 
 
