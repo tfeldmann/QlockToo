e1d9ebeaf66bb9f8ee2111028d3e89d57e53a2bc
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-04-09 23:06:35 +0200

    added letter layout to simulator

--- Software/simulator.py
+++ Software/simulator.py
@@ -4,13 +4,25 @@ QlockTwo Simulator
 """
 
 import sys, random
-from PySide.QtGui import QApplication, QWidget, QPainter, QColor
+from PySide.QtGui import QApplication, QWidget, QPainter, QColor, QFont
 from PySide.QtCore import Qt
 
 class Simulator(QWidget):
     def __init__(self):
         super(Simulator, self).__init__()
-        self.matrix = [[0]*11]*10
+        self.matrix = [[1]*11]*10
+        # why must the umlaute be unicode?!?
+        self.letters = [
+            ['E', 'S', 'K', 'I', 'S', 'T', 'A', 'F', u'Ü', 'N', 'F'],
+            ['Z', 'E', 'H', 'N', 'Z', 'W', 'A', 'N', 'Z', 'I', 'G'],
+            ['D', 'R', 'E', 'I', 'V', 'I', 'E', 'R', 'T', 'E', 'L'],
+            ['V', 'O', 'R', 'F', 'U', 'N', 'K', 'N', 'A', 'C', 'H'],
+            ['H', 'A', 'L', 'B', 'A', 'E', 'L', 'F', u'Ü', 'N', 'F'],
+            ['E', 'I', 'N', 'S', 'X', u'Ä', 'M', 'Z', 'W', 'E', 'I'],
+            ['D', 'R', 'E', 'I', 'A', 'U', 'J', 'V', 'I', 'E', 'R'],
+            ['S', 'E', 'C', 'H', 'S', 'N', 'L', 'A', 'C', 'H', 'T'],
+            ['S', 'I', 'E', 'B', 'E', 'N', 'Z', 'W', u'Ö', 'L', 'F'],
+            ['Z', 'E', 'H', 'N', 'E', 'U', 'N', 'K', 'U', 'H', 'R']]
         self.initUI()
 
     def initUI(self):
@@ -29,12 +41,14 @@ class Simulator(QWidget):
 
         qp = QPainter()
         qp.begin(self)
-        qp.setPen(Qt.red)
+        qp.fillRect(0, 0, size.width(), size.height(), Qt.black)
+        qp.setFont(QFont('Helvetica', letter_width*0.5))
         for y, row in enumerate(self.matrix):
             for x, cell in enumerate(row):
                 brightness = 255 * cell
-                qp.fillRect(x*letter_width, y*letter_height,
-                    letter_width, letter_height, QColor(*[brightness]*3))
+                qp.setPen(QColor(*[brightness]*3))
+                qp.drawText(x*letter_width, y*letter_height, letter_width, letter_height,
+                    Qt.AlignCenter, self.letters[y][x])
         qp.end()
 
 
