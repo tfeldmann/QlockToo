9c73cfb3154bbdcbcf373e2cb5e0368f461ab84d
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-04-09 20:50:20 +0200

    better reference handling in console app

--- Software/Console/app.py
+++ Software/Console/app.py
@@ -7,19 +7,21 @@ Send and read serial data from the clock
 
 from PySide.QtGui import *
 from PySide.QtCore import *
-from ui_console import Ui_console
+from ui_console import Ui_console as Ui
 
 class ConsoleApp(QDialog):
-    def __init__(self, parent=None):
-        super(ConsoleApp, self).__init__(parent)
-        self.ui = Ui_console()
+    def __init__(self, device):
+        super(ConsoleApp, self).__init__()
+        self.device = device
+        self.ui = Ui()
         self.ui.setupUi(self)
+        self.ui.command.returnPressed.connect(self.sendCommand)
+        self.exec_()
 
-        self.ui.command.returnPressed.connect(self.send_command)
-        self.show()
-
-    def send_command(self):
+    def sendCommand(self):
         cmd = self.ui.command.text()
         if cmd:
             self.ui.command.clear()
-            self.ui.log.append(cmd)
+            self.ui.log.append('<b>Gesendet:</b> <i>'+cmd+'</i>')
+
+
