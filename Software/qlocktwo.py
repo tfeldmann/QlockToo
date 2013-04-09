f51b80c628fdfbeb455fa8b300cd2d98e0c9286b
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-04-09 21:06:30 +0200

    added port selection

--- Software/qlocktwo.py
+++ Software/qlocktwo.py
@@ -3,7 +3,7 @@
 QlockTwo
 
 Software for controlling our self-built QlockTwo remake
-@author Thomas Feldmann
+Author: Thomas Feldmann
 """
 
 from PySide.QtGui import *
@@ -17,21 +17,26 @@ from ui_qlocktwo import Ui_qlocktwo as Ui
 
 
 class QlockTwo(QMainWindow):
+    """
+    The QlockTwo software suite
+
+    Provides links to the available apps and manages the serial devices and
+    simulator.
+    """
+
     def __init__(self):
-        " Initialize the main application "
+        " Initializes the application's UI "
         super(QlockTwo, self).__init__()
-        self.ui = Ui_qlocktwo()
+        self.ui = Ui()
         self.ui.setupUi(self)
 
-        self.ui.refresh.clicked.connect(self.refresh_ports)
-        self.ui.pattern.clicked.connect(self.pattern_app)
-        self.ui.console.clicked.connect(self.console_app)
+        self.refreshPorts()
 
-        self.refresh_ports()
-        self.ui.appbox.setEnabled(True)  # comment if not needed
+        self.ui.refresh.clicked.connect(self.refreshPorts)
+        self.ui.pattern.clicked.connect(self.startPattern)
+        self.ui.console.clicked.connect(self.startConsole)
+        self.ui.port.currentIndexChanged.connect(self.portSelect)
 
-    def refresh_ports(self):
-        " Refreshed the port list "
     def startPattern(self):
         " Starts the pattern app "
         app = PatternApp(device=self.device)
@@ -40,21 +45,38 @@ class QlockTwo(QMainWindow):
         " Start the console app "
         app = ConsoleApp(device=self.device)
 
+    def refreshPorts(self):
+        " refreshes the port list "
         self.ui.port.clear()
         self.ui.port.addItem('Getrennt')
+        self.ui.port.addItem('Simulator')
+        for port in SerialConnection.list_ports():
+            self.ui.port.addItem(port)
 
+    def portSelect(self, index):
+        """
+        Connects / disconnects a device
 
-    def port_select(self, index):
-        # disconnect
-        if index == 0:
-            self.serial.stop()
-        # connect
-        else:
+        This method is called when the user selected a device in the ports
+        combo box. The Device can be either the simulator or a serial device.
+        """
+        # disconnect device / stop simulator
+        self.device = None
+        self.ui.appbox.setEnabled(False)
+
+        if index == 1:
+            # connect to the simulator
+            self.device = Simulator()
+            self.ui.appbox.setEnabled(True)
+
+        if index > 1:
+            # connect to actual QlockTwo remake
             try:
-                port = self.ui.serial_select.currentText()
-                self.serial.connect(port=port, baud=19200)
+                port = self.ui.port.currentText()
+                self.device = Device(port=port, baudrate=57600)
+                self.ui.appbox.setEnabled(True)
             except Exception, e:
-                QMessageBox.warning(self, "Error", e.message)
+                QMessageBox.warning(self, "Error: ", e.message)
 
 
 if __name__ == "__main__":
