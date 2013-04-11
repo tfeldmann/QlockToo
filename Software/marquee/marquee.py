7368b48c0253f071a32852e523d9911faf23d18d
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-04-11 23:20:22 +0200

    show marquee char at cursor position

--- Software/marquee/marquee.py
+++ Software/marquee/marquee.py
@@ -18,8 +18,9 @@ class MarqueeApp(QDialog):
         self.device = device
         self.ui = Ui()
         self.ui.setupUi(self)
-        self.ui.text.textChanged.connect(self.textChanged)
+        # self.ui.text.textChanged.connect(self.textChanged)
         self.ui.text.returnPressed.connect(self.returnPressed)
+        self.ui.text.cursorPositionChanged.connect(self.cursorPositionChanged)
 
         # load default font
         self.font = self.loadFont('fonts/default.json')
@@ -34,6 +35,12 @@ class MarqueeApp(QDialog):
             except Exception, e:
                 QMessageBox.warning(self, 'Error: ', e.message)
 
+    def cursorPositionChanged(self, old, new):
+        x = (new - 1) * 6
+        marquee = self.renderText(text=self.ui.text.text(), font=self.font)
+        extract = self.matrixExtract(marquee, x)
+        self.device.setMatrix(extract)
+
     def textChanged(self, text):
         x = (self.ui.text.cursorPosition() - 1) * 6
         marquee = self.renderText(text=text, font=self.font)
@@ -45,20 +52,21 @@ class MarqueeApp(QDialog):
         self.marquee = self.marqueeMatrix(text)
         self.showMarquee()
 
-    def renderText(self, text, font, spacing=1):
+    def renderText(self, text, font):
         " Returns a matrix representation of the rendered text "
 
         # gather data
         letter_height = font['letter_height']
         letter_width = font['letter_width']
-        offset = 1  # todo: alignment
+        spacing = 1
+        offset = 1
 
         # build a empty 2d-array of desired size
         height = 10  # display height
         length = (spacing + letter_width) * len(text)
-        # we cannot use [[0]*length]*height here
         # see http://stackoverflow.com/q/15956969/300783
-        matrix = [[0] * length for _ in range(height)]
+        # we cannot use [[0]*length]*height here
+        matrix = [[0]*length for _ in range(height)]
 
         # print letters
         for _x, _c in enumerate(text):
@@ -66,10 +74,11 @@ class MarqueeApp(QDialog):
             x = _x * (letter_width + spacing)
             for y, line in enumerate(c):
                 matrix[y+offset][x:x+letter_width] = line
-
         return matrix
 
     def matrixExtract(self, matrix, x=0):
+        if x < 0:
+            x = 0
         extract = [line[x:x+11] for line in matrix]
         result = [line + [0]*(11 - len(line)) for line in extract]
         return result
