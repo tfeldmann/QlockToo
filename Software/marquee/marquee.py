c14fc8e24c2a4b5f72e0858f5d7d7d99ef79fd72
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-04-11 22:00:16 +0200

    show letters while typing

--- Software/marquee/marquee.py
+++ Software/marquee/marquee.py
@@ -18,15 +18,58 @@ class MarqueeApp(QDialog):
         self.device = device
         self.ui = Ui()
         self.ui.setupUi(self)
-        self.loadFont('fonts/default.json')
+        self.ui.text.textChanged.connect(self.textChanged)
+        self.ui.text.returnPressed.connect(self.returnPressed)
+
+        # load default font
+        self.font = self.loadFont('fonts/default.json')
+        # show empty matrix
+        self.device.setMatrix([[0]*11]*10)
         self.exec_()
 
     def loadFont(self, filepath):
         with open(filepath, 'rb') as fontfile:
             try:
-                self.font = json.loads(fontfile.read())
+                return json.loads(fontfile.read())
             except Exception, e:
                 QMessageBox.warning(self, 'Error: ', e.message)
 
-    def marqueeMatrix(self, text):
-        pass
+    def textChanged(self, text):
+        x = (self.ui.text.cursorPosition() - 1) * 6
+        marquee = self.renderText(text=text, font=self.font)
+        extract = self.matrixExtract(marquee, x)
+        self.device.setMatrix(extract)
+
+    def returnPressed(self):
+        text = self.ui.text.text()
+        self.marquee = self.marqueeMatrix(text)
+        self.showMarquee()
+
+    def renderText(self, text, font, spacing=1):
+        " Returns a matrix representation of the rendered text "
+
+        # gather data
+        letter_height = font['letter_height']
+        letter_width = font['letter_width']
+        offset = 1  # todo: alignment
+
+        # build a empty 2d-array of desired size
+        height = 10  # display height
+        length = (spacing + letter_width) * len(text)
+        # we cannot use [[0]*length]*height here
+        # see http://stackoverflow.com/q/15956969/300783
+        matrix = [[0] * length for _ in range(height)]
+
+        # print letters
+        for _x, _c in enumerate(text):
+            c = font[_c]
+            x = _x * (letter_width + spacing)
+            for y, line in enumerate(c):
+                matrix[y+offset][x:x+letter_width] = line
+
+        return matrix
+
+    def matrixExtract(self, matrix, x=0):
+        extract = [line[x:x+11] for line in matrix]
+        result = [line + [0]*(11 - len(line)) for line in extract]
+        return result
