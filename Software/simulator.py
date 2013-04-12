6af7af7791bfb0d1a9053fd8303deb8e47878895
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-04-12 16:18:52 +0200

    added corners to simulator

--- Software/simulator.py
+++ Software/simulator.py
@@ -11,6 +11,7 @@ class Simulator(QWidget):
     def __init__(self):
         super(Simulator, self).__init__()
         self.matrix = [[1]*11]*10
+        self.corners = [1]*4
         # why must the umlaute be unicode?!?
         self.letters = [
             ['E', 'S', 'K', 'I', 'S', 'T', 'A', 'F', u'Ü', 'N', 'F'],
@@ -30,28 +31,52 @@ class Simulator(QWidget):
         self.setWindowTitle('Simulator')
         self.show()
 
-    def setMatrix(self, m):
-        self.matrix = m
+    def setMatrix(self, matrix):
+        self.matrix = matrix
+        self.update()
+        if not self.isVisible():
+            self.show()
+
+    def setCorners(self, corners):
+        self.corners = corners
         self.update()
         if not self.isVisible():
             self.show()
 
     def paintEvent(self, e):
         size = self.size()
-        letter_width = size.width() / 11
-        letter_height = size.height() / 10
+        ratio = 0.75
+        letter_width = size.width()*ratio / 11
+        letter_height = size.height()*ratio / 10
 
         qp = QPainter()
         qp.begin(self)
         qp.fillRect(0, 0, size.width(), size.height(), Qt.black)
+
+        # letters
         qp.setFont(QFont('Helvetica', letter_width*0.5))
         for y, row in enumerate(self.matrix):
             for x, cell in enumerate(row):
-                brightness = 255 * cell
-                qp.setPen(QColor(*[brightness]*3))
-                qp.drawText(x*letter_width, y*letter_height,
+                color = QColor(*[255 * cell] * 3)
+                qp.setPen(color)
+                qp.drawText(
+                    size.width()*(1-ratio)/2 + x*letter_width,
+                    size.height()*(1-ratio)/2 + y*letter_height,
                     letter_width, letter_height,
-                    Qt.AlignCenter, self.letters[y][x])
+                    Qt.AlignCenter,
+                    self.letters[y][x])
+
+        # corners
+        colors = [QColor(*[255 * b]*3) for b in self.corners]
+        pos_top = (1 - ratio) * size.height() / 4
+        pos_left = (1 - ratio) * size.width() / 4
+        pos_bottom = size.height() - pos_top
+        pos_right = size.width() - pos_left
+        qp.fillRect(pos_left, pos_top, 2, 2, colors[0])
+        qp.fillRect(pos_right, pos_top, 2, 2, colors[1])
+        qp.fillRect(pos_left, pos_bottom, 2, 2, colors[2])
+        qp.fillRect(pos_right, pos_bottom, 2, 2, colors[3])
+
         qp.end()
 
 
