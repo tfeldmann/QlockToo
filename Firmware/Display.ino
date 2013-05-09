8350c4b4af3d2a8b34bae2c63736cfb960c7db91
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-05-09 22:15:20 +0200

    removed timer from display module

--- Firmware/Display.ino
+++ Firmware/Display.ino
@@ -24,54 +24,40 @@ void display_init()
     // init row pins
     for (int i = 0; i < ROWS; i++)
     {
-        pinMode(i, OUTPUT);
-        digitalWrite(i, HIGH);
+        pinMode(rowpins[i], OUTPUT);
+        digitalWrite(rowpins[i], HIGH);
     }
 
     // init leddriver
-    leddriver_begin(B1100000);
-    for (int i = 0; i < 16; i++)
+    leddriver_begin(B1100000);  // = 0x60
+    for (int i = 0; i < COLS; i++)
     {
         leddriver_setPinMode(i, LPM_PWM);
+        leddriver_setPinPWM(i, 100);
     }
 
-    display_startRefreshTimer(1);
+    //display_startRefreshTimer(); @todo: set new timer
 }
 
 int8_t display_enableNextRow()
 {
     volatile static int8_t display_currentRow = 0;
+
+    // disable current line
+    digitalWrite(rowpins[display_currentRow], HIGH);
+
+    // enable next line
     display_currentRow = (display_currentRow + 1) % ROWS;
-    //digitalWrite(rowpins[display_currentRow], LOW);
-    digitalWrite(4, LOW);
+    digitalWrite(rowpins[display_currentRow], LOW);
+
+    // return the current line
     return display_currentRow;
 }
 
 void display_update()
 {
     int8_t row = display_enableNextRow();
-    for (int i = 0; i < COLS; i++)
-    {
-        leddriver_setPinPWM(i, matrix[row][i]);
-    }
-}
-
-void display_startRefreshTimer(uint8_t freq)
-{
-    cli();
-    TCCR1A = 0;  // set entire TCCRA register to 0
-    TCCR1B = 0;  // same for TCCRB
-    TCNT1  = 0;  // initialize counter value to 0
-    OCR1A  = 15625 / freq;  // 15625 is one second
-    TCCR1B |= (1 << WGM12);  // turn on CTC mode
-    TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
-    TCCR1B |= (1 << CS12) | (1 << CS10); // 1024 prescaler
-    sei();
-}
-
-ISR(TIMER1_COMPA_vect)
-{
-    display_update();
-    // @todo: Doesn't work
-    Serial.println("Timer");
+    Serial.println(row);
+    // leddriver_setPinPWM(0, matrix[row][0]);
+    Serial.println("");
 }
