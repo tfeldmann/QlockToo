7e96f76ede94a9d9be2210b34b5ba520340fb929
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-10-14 14:19:16 +0200

    added timer

--- Firmware/Display.ino
+++ Firmware/Display.ino
@@ -2,7 +2,10 @@
 // Display.ino
 //
 
+#include "TimerThree.h"
 #include "globals.h"
+
+#define FPS_TO_PERIOD_US(_x_) (1e6 / _x_)
 
 volatile unsigned char matrix[ROWS][COLS] = {
     {LED_MAX, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -19,16 +22,18 @@ volatile unsigned char matrix[ROWS][COLS] = {
 int8_t rowpins[] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 int8_t cornerpin = 4;
 
+
 void display_init()
 {
-    // init row pins
+    // init row pins (transistors)
+    // a HIGH value means that the row is deactivated
     for (int i = 0; i < ROWS; i++)
     {
         pinMode(rowpins[i], OUTPUT);
         digitalWrite(rowpins[i], HIGH);
     }
 
-    // init leddriver
+    // init i2c leddriver
     leddriver_begin(B1100000);  // = 0x60
     for (int i = 0; i < COLS; i++)
     {
@@ -36,8 +41,20 @@ void display_init()
         leddriver_setPinPWM(i, 100);
     }
 
-    //display_startRefreshTimer(); @todo: set new timer
+    // start display update timer interrupt
+    Timer3.initialize(FPS_TO_PERIOD_US(30));
+    Timer3.attachInterrupt(display_update);
 }
+
+
+void display_update()
+{
+    int8_t row = display_enableNextRow();
+    leddriver_setPinPWM(0, matrix[row][0]);
+
+    digitalWrite(13, !digitalRead(13));
+}
+
 
 int8_t display_enableNextRow()
 {
@@ -52,12 +69,4 @@ int8_t display_enableNextRow()
 
     // return the current line
     return display_currentRow;
-}
-
-void display_update()
-{
-    int8_t row = display_enableNextRow();
-    Serial.println(row);
-    // leddriver_setPinPWM(0, matrix[row][0]);
-    Serial.println("");
 }
