810b2f95aafb081e63ba88098c0abcfb3aceeda8
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-10-14 14:18:57 +0200

    enhanced lowpass

--- Software/qlocktoo/demo/lowpass.py
+++ Software/qlocktoo/demo/lowpass.py
@@ -11,15 +11,15 @@ def lowpass(img):
     # create result image. The corners will not be altered
     result = [x[:] for x in img]
 
-    mask = [1, 2, 1,
-            2, 4, 2,
-            1, 2, 1]
+    mask = [1, 4,  1,
+            4, 12, 4,
+            1, 4,  1]
 
     for x, y in itertools.product(range(1, width - 1), range(1, height - 1)):
         part = (img[y - 1][x - 1:x + 2] +
                 img[y][x - 1:x + 2] +
                 img[y + 1][x - 1:x + 2])
-        masked = 1.0 / 16 * sum(map(lambda x, y: x * y, part, mask))
+        masked = 1.0 / 32 * sum(map(lambda x, y: x * y, part, mask))
         result[y][x] = masked
 
     return result
