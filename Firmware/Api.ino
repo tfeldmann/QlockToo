79b6079de38c303cb64121467a57015296103515
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-10-14 15:42:18 +0200

    added api for switching states

--- Firmware/Api.ino
+++ Firmware/Api.ino
@@ -3,6 +3,7 @@
 //
 
 #include "SerialCommand.h"
+#include "globals.h"
 
 SerialCommand serialCommand;
 
@@ -19,17 +20,10 @@ void api_unknown(const char *command)
 
 void api_init()
 {
-    // serialCommand.addCommand("@time", api_time);
-    // serialCommand.addCommand("@brightness_min", api_brightness_min);
-    // serialCommand.addCommand("@brightness_max", api_brightness_max);
-    // serialCommand.addCommand("@nighttime_begin", api_nighttime_begin);
-    // serialCommand.addCommand("@nighttime_end", api_nighttime_end);
-    // serialCommand.addCommand("@nighttime_brightness", api_nighttime_brightness);
-    // serialCommand.addCommand("@duration_words", api_duration_words);
-    // serialCommand.addCommand("@duration_seconds", api_duration_seconds);
-    // serialCommand.addCommand("@duration_date", api_duration_date);
-    // serialCommand.addCommand("@duration_temperature", api_duration_temperature);
-    // serialCommand.addCommand("@matrix" api_matrix);
+    // States
+    serialCommand.addCommand("@timewords", api_timewords);
+    serialCommand.addCommand("@seconds", api_seconds);
+
     serialCommand.addCommand("@about", api_about);
     serialCommand.setDefaultHandler(api_unknown);
 }
@@ -38,18 +32,28 @@ void api_init()
 // ===========================================================================
 // custom functions:
 
+void api_timewords()
+{
+    STATE_SWITCH(TIMEWORDS);
+}
+
+void api_seconds()
+{
+    STATE_SWITCH(SECONDS);
+}
+
 void api_about()
 {
     Serial.println("#QlockToo");
     Serial.println("#   - an open-source remake of the QlockTwo.");
     Serial.println("#===========================================");
     Serial.println("#Firmware Version:");
-    Serial.print("#    "); Serial.println(VERSION);
+    Serial.println("#    " + String(VERSION) + "");
     Serial.println("#");
     Serial.println("#Compiled on:");
-    Serial.print("#    "); Serial.println(__TIMESTAMP__);
+    Serial.println("#    " + String(__TIMESTAMP__) + "");
     Serial.println("#");
-    Serial.println("#Creators:");
+    Serial.println("#Creators: ");
     Serial.println("#    Thomas Feldmann");
     Serial.println("#    Marlene Feldmann");
     Serial.println("#    Manuel Fehmer");
