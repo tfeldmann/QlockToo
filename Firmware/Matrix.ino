d0009e95c0b16bf1bf8e3cdbc408712214f64000
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-10-16 15:36:42 +0200

    added timeword function to matrix module

--- Firmware/Matrix.ino
+++ Firmware/Matrix.ino
@@ -4,23 +4,7 @@
 
 #include "globals.h"
 
-const uint16_t hours[][2] = {
-    // row, mask
-    {5, 0b1111000000000000},  // EINS
-    {5, 0b0000000111100000},  // ZWEI
-    {6, 0b1111000000000000},  // DREI
-    {6, 0b0000000111100000},  // VIER
-    {4, 0b0000000111100000},  // FUENF
-    {7, 0b1111100000000000},  // SECHS
-    {8, 0b1111110000000000},  // SIEBEN
-    {7, 0b0000000111100000},  // ACHT
-    {9, 0b0001111000000000},  // NEUN
-    {9, 0b1111000000000000},  // ZEHN
-    {4, 0b0000011100000000},  // ELF
-    {8, 0b0000000111100000}   // ZWOELF
-};
-
-const uint16_t words[][2] = {
+const uint16_t matrix_words[][2] = {
     // row,mask
     {0, 0b1100000000000000},  // ES
     {0, 0b0001110000000000},  // IST
@@ -34,7 +18,23 @@ const uint16_t words[][2] = {
     {9, 0b0000000011100000}   // UHR
 };
 
-const byte number[][7] = {
+const uint16_t matrix_hours[][2] = {
+    // row, mask
+    {8, 0b0000000111100000},  // ZWOELF
+    {5, 0b1111000000000000},  // EINS
+    {5, 0b0000000111100000},  // ZWEI
+    {6, 0b1111000000000000},  // DREI
+    {6, 0b0000000111100000},  // VIER
+    {4, 0b0000000111100000},  // FUENF
+    {7, 0b1111100000000000},  // SECHS
+    {8, 0b1111110000000000},  // SIEBEN
+    {7, 0b0000000111100000},  // ACHT
+    {9, 0b0001111000000000},  // NEUN
+    {9, 0b1111000000000000},  // ZEHN
+    {4, 0b0000011100000000}   // ELF
+};
+
+const byte matrix_numbers[][7] = {
     {
         0b00001110,   // 0
         0b00010001,
@@ -158,24 +158,50 @@ void matrix_second(uint16_t (&matrix)[ROWS][COLS], int s)
 
 void matrix_timewords(uint16_t (&matrix)[ROWS][COLS], int hour, int minute)
 {
-    // Binary mask for columns:
-    // ES,IST,FÜNF,ZEHN,VIERTEL,ZWANZIG,VOR,NACH,HALB,Stunde,Stunde+1,UHR
+    // Binary mask for 12 columns
+    // ES,IST,FÜNF,ZEHN,VIERTEL,ZWANZIG,VOR,NACH,HALB,UHR, Stunde,Stunde+1
     uint16_t selector[] =
     {
-        0b110000000101,  // 0
-        0b111000010100,  // 5
-        0b110100010100,  // 10
-        0b110010010100,  // 15
-        0b110001010100,  // 20
-        0b111000101010,  // 25
-        0b110000001010,  // 30
-        0b111000011010,  // 35
-        0b110001100010,  // 40
-        0b110010100010,  // 45
-        0b110100100010,  // 50
-        0b111000100010   // 55
-    }
+        0b1100000001100000,  // minute 0
+        0b1110000100100000,  // minute 5
+        0b1101000100100000,  // minute 10
+        0b1100100100100000,  // minute 15
+        0b1100010100100000,  // minute 20
+        0b1110001010010000,  // minute 25
+        0b1100000010010000,  // minute 30
+        0b1110000110010000,  // minute 35
+        0b1100011000010000,  // minute 40
+        0b1100101000010000,  // minute 45
+        0b1101001000010000,  // minute 50
+        0b1110001000010000   // minute 55
+    };
 
     uint16_t action = selector[minute / 5];
 
+    for (int i = 0; i < 12; i++)
+    {
+        if (!bitRead(action, i)) continue;  // selector doesn't apply
+
+        uint8_t row = 0;
+        uint16_t mask = 0;
+
+        if (i == 10) // the current hour
+        {
+            row  = matrix_hours[hour % 12][0];
+            mask = matrix_hours[hour % 12][1];
+        }
+        else if (i == 11) // the next hour
+        {
+            row  = matrix_hours[(hour + 1) % 1][0];
+            mask = matrix_hours[(hour + 1) % 1][1];
+        }
+        else  // first nine selectors mask words
+        {
+            row  = matrix_words[i][0];
+            mask = matrix_words[i][1];
+        }
+
+        // apply mask
+        matrix[row] |= mask;
+    }
 }
