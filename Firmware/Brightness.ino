2fbe26962360d59417de9b2d40e8012edd806b4d
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-10-28 17:34:44 +0100

    formatting modifications to brightness filter

--- Firmware/Brightness.ino
+++ Firmware/Brightness.ino
@@ -5,13 +5,14 @@
 #include "globals.h"
 
 const int LDR_PIN = A0;
-#define FILTER_MAX 4000
+#define FILTER_MAX (5000)  // filter response time in ms (max 65535!)
+
 
 void brightness_update()
 {
-    static unsigned int filtered_value = FILTER_MAX;
-    unsigned int new_sample = map(analogRead(LDR_PIN), 0, 1024, 0, FILTER_MAX);
+    static uint16_t filtered_value = FILTER_MAX;
+    uint16_t new_sample = map(analogRead(LDR_PIN), 0, 1024, 0, FILTER_MAX);
 
     new_sample > filtered_value ? filtered_value++ : filtered_value--;
-    BRIGHTNESS = map(filtered_value, 0, FILTER_MAX, 0, 255);
+    BRIGHTNESS = map(filtered_value, 0, FILTER_MAX, 0, BRIGHTNESS_MAX);
 }
