92fb80fd425a5d43822af36a8c210e0cf9c09944
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-10-28 11:00:18 +0100

    cleanup

--- Firmware/globals.h
+++ Firmware/globals.h
@@ -6,6 +6,9 @@
 #include <Arduino.h>
 #include "statemachine.h"
 
+// Undefine this to enable logging output
+#define DEBUG
+
 // Clock time
 volatile unsigned char seconds, minutes, hours, day, month;
 volatile unsigned int year;
@@ -19,15 +22,6 @@ volatile unsigned int year;
 #define LED_MIN 0
 #define LED_MAX 255
 
-// LedDriver Module PinMode Definitions
-#define LPM_OFF    0
-#define LPM_ON     1
-#define LPM_PWM    2
-#define LPM_GRPPWM 3
-
-// Led Pin
-#define LED_PIN 13
-
 // List of device states
 STATES(
     TIMEWORDS,
@@ -36,16 +30,5 @@ STATES(
     TEMPERATURE
 );
 
-uint8_t matrix[ROWS][COLS] = {  // needs to be volatile?
-    {LED_MAX, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, LED_MAX, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, LED_MAX, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, LED_MAX, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, LED_MAX, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, LED_MAX, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, LED_MAX, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, LED_MAX, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, LED_MAX, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, LED_MAX, 0},
-};
+uint8_t matrix[ROWS][COLS];
 uint8_t corner[4] = {0, 0, 0, 0};
