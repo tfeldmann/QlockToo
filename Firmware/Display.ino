70e3d6e782b627925c758af07342819897db7dab
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-10-28 11:08:44 +0100

    refactoring for display_update

--- Firmware/Display.ino
+++ Firmware/Display.ino
@@ -2,17 +2,21 @@
 // Display.ino
 //
 
-#include "TimerThree.h"
+#include <TimerThree.h>
+#include <I2C.h>
 #include "globals.h"
 
 #define FPS_TO_PERIOD_US(_x_) (1e6 / _x_)
+#define LEDDRIVER_ADDRESS (0x60)
 
-int8_t rowpins[] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+const int8_t rowpins[ROWS] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4};
 int8_t cornerpin = 4;  // outputs 12 13 14 15
 
 
 void display_init()
 {
+    matrix_clear(matrix);
+
     // init row pins (transistors)
     // a HIGH value means that the row is deactivated
     for (int i = 0; i < ROWS; i++)
@@ -21,40 +25,62 @@ void display_init()
         digitalWrite(rowpins[i], HIGH);
     }
 
-    // init i2c leddriver
-    leddriver_begin(0b1100000);  // = 0x60
-    for (int i = 0; i < COLS; i++)
-    {
-        leddriver_setPinMode(i, LPM_PWM);
-        leddriver_setPinPWM(i, 100);
-    }
+    I2c.begin();
+    I2c.setSpeed(true);  // fast
+    I2c.pullup(false);
+
+    uint8_t data[] = {0x00,0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x00, 0xAA, 0xAA, 0xAA, 0xAA, 0x00, 0x00, 0x00, 0x00, 0xFF};
+    I2c.write(LEDDRIVER_ADDRESS, 0x80, data, 29);
 
     // start display update timer interrupt
-    Timer3.initialize(FPS_TO_PERIOD_US(30));
+    Timer3.initialize(FPS_TO_PERIOD_US(1000));
     Timer3.attachInterrupt(display_update);
 }
 
 
 void display_update()
 {
-    int8_t row = display_enableNextRow();
-    // leddriver_setPinPWM(0, matrix[row][0]);
+    controller_update();
 
-    digitalWrite(13, !digitalRead(13));
-}
-
-
-int8_t display_enableNextRow()
-{
-    volatile static int8_t display_currentRow = 0;
+    // maps led columns to positions in data array
+    const static int8_t i2c_led_position[COLS] = {
+        14, 15, 12, 13, 10, 11, 8, 9, 6, 7, 4
+    };
+    volatile static int8_t row = 0;
+    static uint8_t data[16];
 
     // disable current line
-    digitalWrite(rowpins[display_currentRow], HIGH);
+    digitalWrite(rowpins[row], HIGH);
+
+    // advance line index
+    row = (row + 1) % ROWS;
+
+    // clear and fill data array
+    for (int i = 0; i < 16; i++)
+    {
+        data[i] = 0;
+    }
+    for (int i = 0; i < COLS; i++)
+    {
+        data[i2c_led_position[i]] = matrix[row][i];
+    }
+
+    // write data to led driver
+    I2c.write(LEDDRIVER_ADDRESS, 0x82, data, 16);
 
     // enable next line
-    display_currentRow = (display_currentRow + 1) % ROWS;
-    digitalWrite(rowpins[display_currentRow], LOW);
+    digitalWrite(rowpins[row], LOW);
+}
 
-    // return the current line
-    return display_currentRow;
+void dump_matrix()
+{
+    for (int y = 0; y < ROWS; y++)
+    {
+        for (int x = 0; x < COLS; x++)
+        {
+            Serial.print(matrix[y][x]);
+            Serial.print(" ");
+        }
+        Serial.println();
+    }
 }
