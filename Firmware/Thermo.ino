33bbeb4b0c7106a48d997ebc9f52c511681914cf
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-12-11 16:23:01 +0100

    refactored into font.h

--- Firmware/Thermo.ino
+++ Firmware/Thermo.ino
@@ -2,80 +2,10 @@
 // Thermo.ino
 //
 
+#include "font.h"
+
 const int THERMO_PIN = A1;
 
-const uint8_t thermo_numbers[][5] = {
-    {
-        0b11100000,  // 0
-        0b10100000,
-        0b10100000,
-        0b10100000,
-        0b11100000,
-    },
-    {
-        0b00100000,  // 1
-        0b01100000,
-        0b00100000,
-        0b00100000,
-        0b00100000,
-    },
-    {
-        0b11100000,  // 2
-        0b00100000,
-        0b11100000,
-        0b10000000,
-        0b11100000,
-    },
-    {
-        0b11100000,  // 3
-        0b00100000,
-        0b01100000,
-        0b00100000,
-        0b11100000,
-    },
-    {
-        0b10000000,  // 4
-        0b10100000,
-        0b11100000,
-        0b00100000,
-        0b00100000,
-    },
-    {
-        0b11100000,  // 5
-        0b10000000,
-        0b11100000,
-        0b00100000,
-        0b11100000,
-    },
-    {
-        0b11100000,  // 6
-        0b10000000,
-        0b11100000,
-        0b10100000,
-        0b11100000,
-    },
-    {
-        0b11100000,  // 7
-        0b00100000,
-        0b01000000,
-        0b01000000,
-        0b01000000,
-    },
-    {
-        0b11100000,  // 8
-        0b10100000,
-        0b11100000,
-        0b10100000,
-        0b11100000,
-    },
-    {
-        0b11100000,  // 9
-        0b10100000,
-        0b11100000,
-        0b00100000,
-        0b00100000,
-    },
-};
 
 
 float thermo_celsius()
@@ -86,12 +16,68 @@ float thermo_celsius()
 
 void thermo_display(byte temp)
 {
+    // ES LACHT NE KUH
+    // matrix_clear();
+    // matrix[0][0] = 1;
+    // matrix[0][1] = 1;
+    // matrix[7][6] = 1;
+    // matrix[7][7] = 1;
+    // matrix[7][8] = 1;
+    // matrix[7][9] = 1;
+    // matrix[7][10] = 1;
+    // matrix[9][3] = 1;
+    // matrix[9][4] = 1;
+    // matrix[9][7] = 1;
+    // matrix[9][8] = 1;
+    // matrix[9][9] = 1;
+
+    // return;
+
+    // // settings
+    // const byte TOP = 3;
+    // const byte HEIGHT = 5;
+    // const byte WIDTH = 3;
+    // const byte LEFT_1 = 1;
+    // const byte LEFT_2 = 5;
+
+    // // split second in left and right part
+    // byte t_1 = temp / 10;
+    // byte t_2 = temp % 10;
+
+    // for (byte y = 0; y < ROWS; y++)
+    // {
+    //     for (byte x = 0; x < COLS; x++)
+    //     {
+    //         // the rows
+    //         if (y >= TOP && y < TOP + HEIGHT)
+    //         {
+    //             // first letter
+    //             if (x >= LEFT_1 && x < LEFT_1 + WIDTH)
+    //             {
+    //                 matrix[y][x] = bitRead(thermo_numbers[t_1][y - TOP],
+    //                                        7 - x + LEFT_1);
+    //             }
+    //             // second letter
+    //             else if (x >= LEFT_2 && x < LEFT_2 + WIDTH)
+    //             {
+    //                 matrix[y][x] = bitRead(thermo_numbers[t_2][y-TOP],
+    //                                        7 - x + LEFT_2);
+    //             }
+    //             // clear the rest
+    //             else matrix[y][x] = 0;
+    //         }
+    //         else matrix[y][x] = 0;
+    //     }
+    // }
+    //
+
+
     // settings
-    const byte TOP = 3;
-    const byte HEIGHT = 5;
-    const byte WIDTH = 3;
-    const byte LEFT_1 = 4;
-    const byte LEFT_2 = 8;
+    const byte TOP = 2;
+    const byte HEIGHT = 6;
+    const byte WIDTH = 4;
+    const byte LEFT_1 = 0;
+    const byte LEFT_2 = 5;
 
     // split second in left and right part
     byte t_1 = temp / 10;
@@ -107,13 +93,13 @@ void thermo_display(byte temp)
                 // first letter
                 if (x >= LEFT_1 && x < LEFT_1 + WIDTH)
                 {
-                    matrix[y][x] = bitRead(thermo_numbers[t_1][y - TOP],
+                    matrix[y][x] = bitRead(numbers4x6[t_1][y - TOP],
                                            7 - x + LEFT_1);
                 }
                 // second letter
                 else if (x >= LEFT_2 && x < LEFT_2 + WIDTH)
                 {
-                    matrix[y][x] = bitRead(thermo_numbers[t_2][y-TOP],
+                    matrix[y][x] = bitRead(numbers4x6[t_2][y-TOP],
                                            7 - x + LEFT_2);
                 }
                 // clear the rest
@@ -122,4 +108,9 @@ void thermo_display(byte temp)
             else matrix[y][x] = 0;
         }
     }
+
+    matrix[0][9] = 1;
+    matrix[0][10] = 1;
+    matrix[1][9] = 1;
+    matrix[1][10] = 1;
 }
