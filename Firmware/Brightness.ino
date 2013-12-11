c4a913ef6d1f774f2c9204d136d32be7fa3a9847
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-12-11 14:48:51 +0100

    simplified brightness

--- Firmware/Brightness.ino
+++ Firmware/Brightness.ino
@@ -5,31 +5,33 @@
 #include "globals.h"
 
 #define LDR_PIN A0
-const uint8_t STEPS = 7;
-const uint8_t step[STEPS] = {255, 220, 200, 150, 100, 50, 0};
+const uint8_t STEPS = 8;
+const uint8_t step[STEPS] = {-1, 255, 220, 200, 150, 100, 50, 0};
 
-static bool automatic = true;
-static uint8_t index = STEPS;  // start in automatic mode
+#define AUTOMATIC 0
+static uint8_t mode = AUTOMATIC;  // start in automatic mode
 
 
 void brightness_update()
 {
-    if (automatic)
+    if (mode == AUTOMATIC)
     {
         uint16_t value = analogRead(LDR_PIN);
         brightness = map(value, 0, 600, 5, 255);
     }
     else
     {
-        brightness = step[index];
+        brightness = step[mode];
     }
 }
 
 
 void brightness_next_step()
 {
-    // STEPS is the length of the array. Index walks over all steps plus one,
-    // the additional index mode is the automatic mode.
-    index = (index + 1) % (STEPS + 1);
-    automatic = (index == STEPS);
+    mode = (mode + 1) % STEPS;
+}
+
+void brightness_enable_automatic()
+{
+    mode = AUTOMATIC;
 }
