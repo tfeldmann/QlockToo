35b7a40c99174fb4ee76f91e773c3c304d595676
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-12-11 14:13:04 +0100

    formatting

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -4,37 +4,50 @@
 // / /_/ / / / /_/ / /__/ ,<  / / / /_/ / /_/ /
 // \___\_\/_/\____/\___/_/|_|/_/  \____/\____/
 //
-// Firmware entry point for the QlockToo, an open-source remake of the QlockTwo.
+// Firmware entry point for the QlockToo, an open-source remake of the QlockTwo
 // Author: Thomas Feldmann
 //
-// Timer1: keeps the time
-// Timer3: updates the display
+// Timer1:    1/s   keeps the time
+// Timer3: 1000/s   main timer for display / api / controller
 
-#define VERSION "0.5"
-#define BAUDRATE 115200
-
-#include <Bounce.h>
 #include "globals.h"
+#include <TimerThree.h>
+#define VERSION "0.6"
 
 
 void setup()
 {
-    Serial.begin(BAUDRATE);
+    Serial.begin(115200);
+
+    // initialize components
     api_init();
     time_init();
     display_init();
-    button_init();
     controller_init();
+
+    // start main timer
+    Timer3.initialize(1e6 / 1000);  // fps to us
+    Timer3.attachInterrupt(main_timer);
+}
+
+
+void main_timer()
+{
+    static uint8_t ms = 0;
+
+    // update display
+    display_update();
+
+    // update controller every 100ms
+    if (++ms == 100)
+    {
+        controller_update();
+        ms = 0;
+    }
 }
 
 
 void loop()
 {
     api_update();
-
-    button_update();
-    if (button1.risingEdge()) STATE_SWITCH(STATE_TIMEWORDS);
-    if (button2.risingEdge()) STATE_SWITCH(STATE_SECONDS);
-    if (button3.risingEdge()) STATE_SWITCH(STATE_TEMPERATURE);
-    if (button4.risingEdge()) brightness_next_step();
 }
